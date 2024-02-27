fa2ef85e0c9d2af4a48fb8d7f617d72125a476c9
jPraiser23 <praiserjohn@gmail.com>
2024-02-27 11:46:46 -0500

    Logout and GarageList

--- app/src/main/java/com/example/carpark_uc/Home.java
+++ app/src/main/java/com/example/carpark_uc/Home.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -12,7 +17,8 @@ import java.util.List;
 public class Home extends AppCompatActivity {
     String [] garageNames = {"CCM","Garage 2", "Garage 3"};
     int counter = 0 ;
-
+    private FirebaseAuth mAuth;
+    private Button BtnLogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +32,17 @@ public class Home extends AppCompatActivity {
         GarageListAdapter adapter = new GarageListAdapter(items);
         recyclerView.setAdapter(adapter);
 
+        mAuth = FirebaseAuth.getInstance();
+        BtnLogout = findViewById(R.id.logoutBtn);
+        BtnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent intent = new Intent(Home.this,Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
 }
